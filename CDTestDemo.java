3e50370066164f817103a72e9b494579cb6a90ce
Prasadgvd1990 <53961024+Prasadgvd1990@users.noreply.github.com>
2024-11-08 19:55:47 +0530

    Add files via upload

---
+++ CDTestDemo.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CDTestDemo {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[] { 0, 5, 1, 7, 3, 5, 0, 7, 0, 4 };
+		
+		
+		int toptwo=Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).distinct().skip(1).limit(1).findFirst().get();
+		System.out.println(toptwo);
+
+		// 0025173574
+
+//		for (int i = 1; i < arr.length; i++) {
+//			if (arr[i] == 0) {
+//				for (int j = i - 1; j >= 0; j--) {
+//					arr[j + 1] = arr[j];
+//					if (j == 0 || arr[j - 1] == 0) {
+//						arr[j] = 0;
+//						break;
+//					}
+//
+//				}
+//				
+//			}
+//		}
+//		for (int val : arr) {
+//			System.out.print(val);
+//		}
+
+	}
+
+}
