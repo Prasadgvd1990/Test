68cbb1d2f6b0d06ddd97e6f5a6a9bc19691a796e
Prasadgvd1990 <53961024+Prasadgvd1990@users.noreply.github.com>
2024-11-11 14:35:32 +0530

    Update CDTestDemo.java

--- CDTestDemo.java
+++ CDTestDemo.java
@@ -7,7 +7,15 @@ public class CDTestDemo {
 	public static void main(String[] args) {
 
 		int[] arr = new int[] { 0, 5, 1, 3, 0, 7, 0, 4 };
-		Arrays.sort(arr);
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
 		System.out.println(arr[arr.length-2]);
 		
 		//int toptwo=Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).distinct().skip(1).limit(1).findFirst().get();
